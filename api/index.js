6769b84ba5cc141d4eb6fbeb8d18d81c98f5c177
Connor Davis <19422@kloofhigh.co.za>
2021-05-12 20:30:15 +0200

    take away test stuff

--- api/index.js
+++
@@ -1,8 +0,0 @@
-let { Router } = require('express');
-let router = Router();
-
-router.get('/', async (request, response) => {
-  return response.status(200).send('Welcome to the API.');
-});
-
-module.exports = router;
