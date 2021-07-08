c0c55c7778b66b1004414c395ce195e8cfd411e4
Connor Davis <19422@kloofhigh.co.za>
2021-07-08 20:41:39 +0200

    fix keys

--- client-dev.js
+++ client-dev.js
@@ -2,5 +2,5 @@ let { DropZoneClient } = require('.');
 let openports = require('openports');
 
 openports(1, async (error, ports) => {
-  let client = new DropZoneClient({ key: 'test', port: ports[0] });
+  let client = new DropZoneClient({ key: 'fe1c07ad89390d67f6eb199e2f89547d5d7ee7f894ca6b4f6b0b7b4fbb64a1f8', port: ports[0] });
 });
