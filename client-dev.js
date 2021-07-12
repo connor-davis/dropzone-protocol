d67b7ae6db3c55278aa51928a417c5cad5b3e5c0
Connor Davis <19422@kloofhigh.co.za>
2021-07-12 11:48:25 +0200

    update client to test connector

--- client-dev.js
+++ client-dev.js
@@ -3,7 +3,7 @@ let openports = require('openports');
 
 openports(1, async (error, ports) => {
   let client = new DropZoneClient({
-    key: 'fe1c07ad89390d67f6eb199e2f89547d5d7ee7f894ca6b4f6b0b7b4fbb64a1f8',
+    key: 'df9844b69d5d5957826f6bea936b64e4ef7408879bb92a1204437c9940fa9a1a',
     port: ports[0],
   });
 });
